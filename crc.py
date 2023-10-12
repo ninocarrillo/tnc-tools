0b1d8588a137cfaab2bb1f76c469ffd451df515a
Nino Carrillo <nino.carrillo@outlook.com>
2023-10-11 20:14:48 -0400

    update CalcCRC16

--- crc.py
+++ crc.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 def CalcCRC16(packet):
-	#packet_val = packet[byte_count - 1] * 256
-	#packet_val += packet[byte_count - 2]
-	# print(hex(packet_val))
-	#packet = packet[0:byte_count - 2] # slicing exludes the end index
 	fcsval = np.uint16(0xFFFF)
 	CRC_poly = np.uint16(0x8408)
 	one = np.uint16(1)
@@ -16,7 +12,4 @@ def CalcCRC16(packet):
 				fcsval = np.bitwise_xor(fcsval, CRC_poly)
 			byte = np.right_shift(byte, 1)
 	fcs_val = np.bitwise_and(np.bitwise_not(fcsval), 0xFFFF)
-	if packet_val == fcs_val:
-		return [fcs_val, 1]
-	else:
-		return [fcs_val, 0]
+	return(fcs_val)
